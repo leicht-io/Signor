a5dd2e5aad574d25067754ab0f0bb1d424c9923e
cle <christian@leicht.io>
2022-03-20 20:29:19 +0100

    feat: initial commit.

--- app/src/main/java/com/example/signor/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/signor/ui/dashboard/DashboardFragment.kt
@@ -2,7 +2,6 @@ package com.example.signor.ui.dashboard
 
 import android.media.AudioAttributes
 import android.media.AudioFormat
-import android.media.AudioManager
 import android.media.AudioTrack
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -11,7 +10,6 @@ import android.view.ViewGroup
 import android.widget.Button
 import androidx.fragment.app.Fragment
 import com.example.signor.databinding.FragmentDashboardBinding
-import com.github.nisrulz.zentone.ZenTone
 import java.lang.Math.PI
 
 
@@ -23,8 +21,7 @@ class DashboardFragment : Fragment() {
     val buffLength: Int = AudioTrack.getMinBufferSize(Fs, AudioFormat.CHANNEL_OUT_MONO, AudioFormat.ENCODING_PCM_16BIT)
 
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
+    // This property is only valid between onCreateView and onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -38,21 +35,10 @@ class DashboardFragment : Fragment() {
         val playBtn: Button = binding.PlayBtn;
         playBtn.setOnClickListener {
             if (!isPlaying) {
-                // Create a new thread to play the audio.
-
-                // Performing intensive operations and computations on the main UI thread,
-                // makes the app slow.
-
-                // That is, it is a bad idea to do intensive computations on main UI thread,
-                // so it is recommended to create a new thread to do computations in the background
-
                 Thread {
-                    val zenTone = ZenTone(channelMask = AudioFormat.CHANNEL_OUT_STEREO)
-                    zenTone.play(frequency = 30f, volume = 2)
-
-                    /* initTrack()
+                    initTrack()
                     startPlaying()
-                    playback() */
+                    playback()
 
                 }.start()
             }
@@ -86,27 +72,7 @@ class DashboardFragment : Fragment() {
             .build()
     }
 
-    private fun generateTone(freqHz: Double, durationMs: Int): AudioTrack? {
-        val count = (44100.0 * 2.0 * (durationMs / 1000.0)).toInt() and 1.inv()
-        val samples = ShortArray(count)
-        var i = 0
-        while (i < count) {
-            val sample = (Math.sin(2 * PI * i / (44100.0 / freqHz)) * 0x7FFF).toInt().toShort()
-            samples[i + 0] = sample
-            samples[i + 1] = sample
-            i += 2
-        }
-        val track = AudioTrack(
-            AudioManager.STREAM_MUSIC, 44100,
-            AudioFormat.CHANNEL_OUT_STEREO, AudioFormat.ENCODING_PCM_16BIT,
-            count * (java.lang.Short.SIZE / 8), AudioTrack.MODE_STATIC
-        )
-        track.write(samples, 0, count)
-        return track
-    }
-
     private fun playback() {
-        // simple sine wave generator
         val frame_out: ShortArray = ShortArray(buffLength)
         val amplitude: Int = 32767
         val frequency: Int = 30
