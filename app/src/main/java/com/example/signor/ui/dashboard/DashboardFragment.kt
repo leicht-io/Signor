660b6fe07ba095a5531ed4ca4f6135f5e4b91556
cle <cle@skov.com>
2022-03-24 14:59:34 +0100

    chore:

--- app/src/main/java/com/example/signor/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/signor/ui/dashboard/DashboardFragment.kt
@@ -1,5 +1,6 @@
 package com.example.signor.ui.dashboard
 
+import android.graphics.Color
 import android.media.AudioAttributes
 import android.media.AudioFormat
 import android.media.AudioTrack
@@ -11,26 +12,39 @@ import android.widget.Button
 import androidx.fragment.app.Fragment
 import com.example.signor.databinding.FragmentDashboardBinding
 import java.lang.Math.PI
+import kotlin.math.sin
 
 class DashboardFragment : Fragment() {
-    lateinit var Track: AudioTrack
-    private var _binding: FragmentDashboardBinding? = null
-    var isPlaying: Boolean = false
-    val Fs: Int = 44100
-    val buffLength: Int = AudioTrack.getMinBufferSize(Fs, AudioFormat.CHANNEL_OUT_MONO, AudioFormat.ENCODING_PCM_16BIT)
+    private lateinit var audioTrack: AudioTrack
 
-    // This property is only valid between onCreateView and onDestroyView.
-    private val binding get() = _binding!!
+    private val sampleRate: Int = 44100
+    private val bufferLength: Int = AudioTrack.getMinBufferSize(
+        sampleRate,
+        AudioFormat.CHANNEL_OUT_MONO,
+        AudioFormat.ENCODING_PCM_16BIT
+    )
+
+    private var fragmentDashboardBinding: FragmentDashboardBinding? = null
+    private var isPlaying: Boolean = false
+
+    private val binding get() = fragmentDashboardBinding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        fragmentDashboardBinding = FragmentDashboardBinding.inflate(inflater, container, false)
         val root: View = binding.root
-
         val playBtn: Button = binding.PlayBtn;
+
+        binding.siriView.apply {
+            updateSpeaking(false)
+            updateViewColor(Color.BLACK)
+            updateAmplitude(0.5f)
+            updateSpeed(-0.1f)
+        }
+
         playBtn.setOnClickListener {
             if (!isPlaying) {
                 Thread {
@@ -39,20 +53,24 @@ class DashboardFragment : Fragment() {
                     playback()
 
                 }.start()
+
+                binding.siriView.apply {
+                    updateSpeaking(true)
+                }
+            } else {
+                stopPlaying()
+
+                binding.siriView.apply {
+                    updateSpeaking(true)
+                }
             }
-        }
-
-
-        val stopBtn: Button = binding.StopBtn;
-
-        stopBtn.setOnClickListener {
-            stopPlaying()
         }
 
         return root
     }
+
     private fun initTrack() {
-        Track = AudioTrack.Builder()
+        audioTrack = AudioTrack.Builder()
             .setAudioAttributes(
                 AudioAttributes.Builder()
                     .setUsage(AudioAttributes.USAGE_ALARM)
@@ -62,36 +80,36 @@ class DashboardFragment : Fragment() {
             .setAudioFormat(
                 AudioFormat.Builder()
                     .setEncoding(AudioFormat.ENCODING_PCM_16BIT)
-                    .setSampleRate(44100)
+                    .setSampleRate(sampleRate)
                     .setChannelMask(AudioFormat.CHANNEL_OUT_MONO)
                     .build()
             )
-            .setBufferSizeInBytes(Fs)
+            .setBufferSizeInBytes(sampleRate)
             .build()
     }
 
     private fun playback() {
-        val frame_out: ShortArray = ShortArray(buffLength)
-        val amplitude: Int = 32767
-        val frequency: Int = 30
-        val twopi: Double = PI * 2
+        val frameOut: ShortArray = ShortArray(bufferLength)
+        val amplitude: Int = 32767 // Volume
+        val frequency: Int = 30 // 30hz
+        val twoPi: Double = PI * 2
         var phase: Double = 0.0
 
         while (isPlaying) {
-            for (i in 0 until buffLength) {
-                frame_out[i] = (amplitude * Math.sin(phase)).toInt().toShort()
-                phase += twopi * frequency / Fs
-                if (phase > twopi) {
-                    phase -= twopi
+            for (i in 0 until bufferLength) {
+                frameOut[i] = (amplitude * sin(phase)).toInt().toShort()
+                phase += twoPi * frequency / sampleRate
+                if (phase > twoPi) {
+                    phase -= twoPi
                 }
             }
 
-            Track.write(frame_out, 0, buffLength)
+            audioTrack.write(frameOut, 0, bufferLength)
         }
     }
 
     private fun startPlaying() {
-        Track.play()
+        audioTrack.play()
         isPlaying = true
     }
 
@@ -99,8 +117,8 @@ class DashboardFragment : Fragment() {
         if (isPlaying) {
             isPlaying = false
 
-            Track.stop()
-            Track.release()
+            audioTrack.stop()
+            audioTrack.release()
         }
     }
 
@@ -108,6 +126,6 @@ class DashboardFragment : Fragment() {
         stopPlaying();
 
         super.onDestroyView()
-        _binding = null
+        fragmentDashboardBinding = null
     }
 }
